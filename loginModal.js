49b0126ca129c30191f35ed8817dbb6149962bfe
Victor Pharsen <vnlo.skp@edu.mercantec.dk>
2020-03-03 12:48:29 +0100

    i finished the javascript, but scrapped it for jquery instead

--- loginModal.js
+++ loginModal.js
@@ -1,28 +1,13 @@
-window.onload = function(){
-    //Get the Modal
-    var modal = document.getElementById("login");
+$(document).ready(function() {
+    $('#loginButton').click(function(){
+        $('#login').css('display', 'block');
+    });
 
-    //Get the registerBtn
-    var loginbtn = document.getElementById("loginButton");
+    $('#login').find('.close').click(function(){
+        $('#login').css('display', 'none');
+    })
 
-    //Get the Span
-    var span = document.getElementsByClassName("close")[0]
-
- //OPENING THE MODAL
-    // When the user clicks on the Registerbutton, open the modal
-    loginbtn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    //When the user clicks on the 'x' marker, close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function(event){
-        if (event.target == modal){
-            modal.style.display = "none";
-        }
-    }
-}
-
+    $('#login').click(function(){
+        $('.modal').css('display', 'none');
+    })
+}); 
--- registerModal.js
+++ registerModal.js
@@ -1,28 +1,14 @@
-window.onload = function(){
-    //Get the Modal
-    var modal = document.getElementById("register");
+$(document).ready(function() {
+    $('#registerButton').click(function(){
+        $('#register').css('display', 'block');
+    });
 
-    //Get the registerBtn
-    var registerbtn = document.getElementById("registerButton");
+    $('#register').find('.close').click(function(){
+        $('#register').css('display', 'none');
+    })
 
-    //Get the Span
-    var span = document.getElementsByClassName("close")[0]
-
- //OPENING THE MODAL
-    // When the user clicks on the Registerbutton, open the modal
-    registerbtn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    //When the user clicks on the 'x' marker, close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function(event){
-        if (event.target == modal){
-            modal.style.display = "none";
-        }
-    }
-}
+    $('#register').click(function(){
+        $('.modal').css('display', 'none');
+    })
+});  
 
